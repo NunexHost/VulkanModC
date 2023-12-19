bb952c8d6d16ae1e852f535aa9e6203c077ab17f
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-19 18:25:58 -0300

    Update UploadBuffer.java

--- src/main/java/net/vulkanmod/render/chunk/build/UploadBuffer.java
+++ src/main/java/net/vulkanmod/render/chunk/build/UploadBuffer.java
@@ -6,45 +6,17 @@ import org.lwjgl.system.MemoryUtil;
 
 import java.nio.ByteBuffer;
 
-/**
- * Represents a buffer that has been uploaded to the GPU.
- */
 public class UploadBuffer {
 
-    /**
-     * The number of indices in the buffer.
-     */
-    public int getIndexCount() { return indexCount; }
+    public final int indexCount;
+    public final boolean autoIndices;
+    public final boolean indexOnly;
+    private final ByteBuffer vertexBuffer;
+    private final ByteBuffer indexBuffer;
 
-    /**
-     * Returns a view of the vertex buffer.
-     *
-     * @throws IllegalStateException if the buffer has not been initialized
-     */
-    public ByteBuffer getVertexBufferView() {
-        if (vertexBuffer == null) {
-            throw new IllegalStateException("Vertex buffer has not been initialized");
-        }
-        return vertexBuffer.asReadOnlyBuffer();
-    }
+    //debug
+    private boolean released = false;
 
-    /**
-     * Returns a view of the index buffer.
-     *
-     * @throws IllegalStateException if the buffer has not been initialized
-     */
-    public ByteBuffer getIndexBufferView() {
-        if (indexBuffer == null) {
-            throw new IllegalStateException("Index buffer has not been initialized");
-        }
-        return indexBuffer.asReadOnlyBuffer();
-    }
-
-    /**
-     * Creates a new `UploadBuffer` instance.
-     *
-     * @param renderedBuffer the rendered buffer to upload
-     */
     public UploadBuffer(TerrainBufferBuilder.RenderedBuffer renderedBuffer) {
         TerrainBufferBuilder.DrawState drawState = renderedBuffer.drawState();
         this.indexCount = drawState.indexCount();
@@ -52,27 +24,31 @@ public class UploadBuffer {
         this.indexOnly = drawState.indexOnly();
 
         if (!this.indexOnly) {
-            this.vertexBuffer = Util.createCopy(renderedBuffer.vertexBuffer());
+            this.vertexBuffer = renderedBuffer.vertexBuffer().view();
         } else {
             this.vertexBuffer = null;
         }
 
         if (!drawState.sequentialIndex()) {
-            this.indexBuffer = Util.createCopy(renderedBuffer.indexBuffer());
+            this.indexBuffer = renderedBuffer.indexBuffer().view();
         } else {
             this.indexBuffer = null;
         }
     }
 
-    /**
-     * Releases all resources associated with the buffer.
-     */
+    public int indexCount() { return indexCount; }
+
+    public ByteBuffer getVertexBuffer() { return vertexBuffer; }
+
+    public ByteBuffer getIndexBuffer() { return indexBuffer; }
+
     public void release() {
         if (vertexBuffer != null) {
-            MemoryUtil.memFree(vertexBuffer);
+            MemoryUtil.memUnmap(vertexBuffer);
         }
         if (indexBuffer != null) {
-            MemoryUtil.memFree(indexBuffer);
+            MemoryUtil.memUnmap(indexBuffer);
         }
+        this.released = true;
     }
 }
