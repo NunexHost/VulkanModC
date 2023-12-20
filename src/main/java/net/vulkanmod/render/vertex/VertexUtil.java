a671f70921aeabe5ca32dde6e5923d8baf3a2a8e
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-20 11:22:17 -0300

    Update VertexUtil.java

--- src/main/java/net/vulkanmod/render/vertex/VertexUtil.java
+++ src/main/java/net/vulkanmod/render/vertex/VertexUtil.java
@@ -3,38 +3,26 @@ package net.vulkanmod.render.vertex;
 public class VertexUtil {
 
     private static final float NORM_INV = 1.0f / 127.0f;
-    private static final float COLOR_INV = 1.0f / 255.0f;
+    private static final float COLOR_SCALE = 1.0f / 255.0f;
 
     public static int packColor(float r, float g, float b, float a) {
-        return (int)(r * 255.0f) | ((int)(g * 255.0f) << 8) | ((int)(b * 255.0f) << 16) | ((int)(a * 255.0f) << 24);
+        return ((int)(r * COLOR_SCALE) & 0xFF) |
+                ((int)(g * COLOR_SCALE) & 0xFF) << 8 |
+                ((int)(b * COLOR_SCALE) & 0xFF) << 16 |
+                ((int)(a * COLOR_SCALE) & 0xFF) << 24;
     }
 
     public static int packNormal(float x, float y, float z) {
-        return (int)(x * 127.0f) | ((int)(y * 127.0f) << 8) | ((int)(z * 127.0f) << 16);
+        return ((int)(x * NORM_INV) & 0xFF) |
+                ((int)(y * NORM_INV) & 0xFF) << 8 |
+                ((int)(z * NORM_INV) & 0xFF) << 16;
     }
 
-    public static float unpackColorR(int i) {
-        return (i >> 24) * COLOR_INV;
+    public static float unpackColor(int i, int component) {
+        return ((i >> (component * 8)) & 0xFF) * COLOR_SCALE;
     }
 
-    public static float unpackColorG(int i) {
-        return (i >> 16) * COLOR_INV;
+    public static float unpackNormal(int i, int component) {
+        return ((i >> (component * 8)) & 0xFF) * NORM_INV;
     }
-
-    public static float unpackColorB(int i) {
-        return (i >> 8) * COLOR_INV;
-    }
-
-    public static float unpackN1(int i) {
-        return (i & 0xFF) * NORM_INV;
-    }
-
-    public static float unpackN2(int i) {
-        return ((i >> 8) & 0xFF) * NORM_INV;
-    }
-
-    public static float unpackN3(int i) {
-        return ((i >> 16) & 0xFF) * NORM_INV;
-    }
-
 }
