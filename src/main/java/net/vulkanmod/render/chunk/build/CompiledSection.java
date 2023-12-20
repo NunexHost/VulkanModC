997c89f48b91088c6114ba4ce6b249f39168dac6
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-19 22:05:38 -0300

    Update CompiledSection.java

--- src/main/java/net/vulkanmod/render/chunk/build/CompiledSection.java
+++ src/main/java/net/vulkanmod/render/chunk/build/CompiledSection.java
@@ -2,58 +2,40 @@ package net.vulkanmod.render.chunk.build;
 
 import com.google.common.collect.Lists;
 import it.unimi.dsi.fastutil.objects.ObjectArraySet;
-import net.minecraft.client.renderer.chunk.VisibilitySet;
+import net.minecraft.client.renderer.chunk.OcclusionConsumer;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.vulkanmod.render.vertex.TerrainBufferBuilder;
 import net.vulkanmod.render.vertex.TerrainRenderType;
 
 import javax.annotation.Nullable;
-import java.util.BitSet;
 import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
 
-public class CompiledSection {
-    public static final CompiledSection UNCOMPILED = new CompiledSection() {
+public class TerrainSection {
+    public static final TerrainSection UNCOMPILED = new TerrainSection() {
         public boolean canSeeThrough(Direction dir1, Direction dir2) {
             return false;
         }
     };
+    public final Set<TerrainRenderType> renderTypes = EnumSet.noneOf(TerrainRenderType.class);
+    boolean isCompletelyEmpty = true;
+    final List<BlockEntity> renderableBlockEntities = Lists.newArrayList();
 
-    public final BitSet renderTypes = new BitSet();
-    public boolean isCompletelyEmpty;
-    public final BlockEntity[] renderableBlockEntities = new BlockEntity[64];
-    public VisibilitySet visibilitySet = new VisibilitySet();
-    @Nullable
-    public TerrainBufferBuilder.SortState transparencyState;
-
-    public CompiledSection() {
-        this.isCompletelyEmpty = true;
-    }
-
-    public void init(EnumSet<TerrainRenderType> renderTypes, List<BlockEntity> renderableBlockEntities) {
-        this.renderTypes.or(renderTypes);
-        this.isCompletelyEmpty = renderTypes.isEmpty();
-
-        for (int i = 0; i < renderableBlockEntities.size(); i++) {
-            this.renderableBlockEntities[i] = renderableBlockEntities.get(i);
-        }
-
-        this.visibilitySet.updateFromBlockEntities(this.renderableBlockEntities);
-    }
-
-    public boolean hasNoRenderableLayers() {
+    public boolean isEmpty() {
         return this.isCompletelyEmpty;
     }
 
     public boolean isEmpty(TerrainRenderType p_112759_) {
-        return !this.renderTypes.get(p_112759_.ordinal());
+        return !this.renderTypes.contains(p_112759_);
     }
 
     public List<BlockEntity> getRenderableBlockEntities() {
-        return Lists.newArrayList(this.renderableBlockEntities);
+        return this.renderableBlockEntities;
     }
 
     public boolean canSeeThrough(Direction dir1, Direction dir2) {
-        return this.visibilitySet.visibilityBetween(dir1, dir2);
+        return new OcclusionConsumer.VisibleFaces(dir1, dir2).apply();
     }
 }
