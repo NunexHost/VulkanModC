fe7bad6fab8a327a4707323b783000a23b25bd61
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-20 00:32:51 -0300

    Update BuildTimeBench.java

--- src/main/java/net/vulkanmod/render/profiling/BuildTimeBench.java
+++ src/main/java/net/vulkanmod/render/profiling/BuildTimeBench.java
@@ -9,26 +9,42 @@ public abstract class BuildTimeBench {
     private static long startTime;
     private static float benchTime;
 
-    public static void runBench(boolean building, TaskDispatcher taskDispatcher) {
-        if(bench) {
-            if (startTime == 0) {
-                startTime = System.nanoTime();
-            }
-
-            if(!building && taskDispatcher.isIdle()) {
-                benchTime = (System.nanoTime() - startTime) * 0.000001f;
-                bench = false;
-                startTime = 0;
-            }
-        }
-    }
-
+    /**
+     * Starts the benchmark timer.
+     */
     public static void startBench() {
         bench = true;
         Minecraft.getInstance().levelRenderer.allChanged();
     }
 
+    /**
+     * Stops the benchmark timer and returns the elapsed time in seconds.
+     *
+     * @return The elapsed time in seconds.
+     */
     public static float getBenchTime() {
-        return benchTime;
+        if (!bench) {
+            return 0.0f;
+        }
+
+        float elapsedTime = (System.nanoTime() - startTime) * 0.000001f;
+        bench = false;
+        startTime = 0;
+
+        return elapsedTime;
+    }
+
+    /**
+     * Runs the benchmark timer.
+     *
+     * @param building Whether the benchmark is being run during the building phase.
+     * @param taskDispatcher The task dispatcher.
+     */
+    public static void runBench(boolean building, TaskDispatcher taskDispatcher) {
+        if (bench && !taskDispatcher.isIdle()) {
+            benchTime = (System.nanoTime() - startTime) * 0.000001f;
+            bench = false;
+            startTime = 0;
+        }
     }
 }
