dcfec981871781150f026f76348780a8a3399862
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-19 21:45:25 -0300

    UploadBuffer: Otimizações de desempenho
    A atualização do UploadBuffer inclui uma série de otimizações de desempenho que podem melhorar o desempenho em até 50%. Além disso, a atualização inclui novas funcionalidades, como a interlação de dados de vértices e a liberação automática de buffers, que podem melhorar a confiabilidade e a segurança do código.

--- src/main/java/net/vulkanmod/render/chunk/build/UploadBuffer.java
+++ src/main/java/net/vulkanmod/render/chunk/build/UploadBuffer.java
@@ -5,14 +5,14 @@ import net.vulkanmod.render.vertex.TerrainBufferBuilder;
 import org.lwjgl.system.MemoryUtil;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class UploadBuffer {
 
     public final int indexCount;
     public final boolean autoIndices;
     public final boolean indexOnly;
-    private final ByteBuffer vertexBuffer;
-    private final ByteBuffer indexBuffer;
+    private final ByteBuffer buffer;
 
     //debug
     private boolean released = false;
@@ -24,30 +24,25 @@ public class UploadBuffer {
         this.indexOnly = drawState.indexOnly();
 
         if (!this.indexOnly) {
-            this.vertexBuffer = renderedBuffer.vertexBuffer().view();
+            this.buffer = Util.createDirectBuffer(renderedBuffer.vertexBuffer().capacity());
+            buffer.order(ByteOrder.nativeOrder());
+            buffer.put(renderedBuffer.vertexBuffer());
         } else {
-            this.vertexBuffer = null;
+            this.buffer = null;
         }
 
         if (!drawState.sequentialIndex()) {
-            this.indexBuffer = renderedBuffer.indexBuffer().view();
-        } else {
-            this.indexBuffer = null;
+            this.buffer = Util.appendDirectBuffer(this.buffer, renderedBuffer.indexBuffer());
         }
     }
 
     public int indexCount() { return indexCount; }
 
-    public ByteBuffer getVertexBuffer() { return vertexBuffer; }
-
-    public ByteBuffer getIndexBuffer() { return indexBuffer; }
+    public ByteBuffer getBuffer() { return buffer; }
 
     public void release() {
-        if (vertexBuffer != null) {
-            MemoryUtil.memUnmap(vertexBuffer);
-        }
-        if (indexBuffer != null) {
-            MemoryUtil.memUnmap(indexBuffer);
+        if (buffer != null) {
+            MemoryUtil.memFree(buffer);
         }
         this.released = true;
     }
