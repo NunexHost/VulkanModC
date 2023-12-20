75c783c2fba20676f0e66701b6d5acccb272c734
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-19 21:51:29 -0300

    Update UploadBuffer.java

--- src/main/java/net/vulkanmod/render/chunk/build/UploadBuffer.java
+++ src/main/java/net/vulkanmod/render/chunk/build/UploadBuffer.java
@@ -5,44 +5,111 @@ import net.vulkanmod.render.vertex.TerrainBufferBuilder;
 import org.lwjgl.system.MemoryUtil;
 
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 
 public class UploadBuffer {
 
+    /**
+     * Número de índices no buffer.
+     */
     public final int indexCount;
+
+    /**
+     * Se os índices são gerados sequencialmente.
+     */
     public final boolean autoIndices;
+
+    /**
+     * Se o buffer contém apenas índices.
+     */
     public final boolean indexOnly;
-    private final ByteBuffer buffer;
 
-    //debug
-    private boolean released = false;
+    /**
+     * Buffer de vértices.
+     */
+    private ByteBuffer vertexBuffer;
 
+    /**
+     * Buffer de índices.
+     */
+    private ByteBuffer indexBuffer;
+
+    /**
+     * Indica se o buffer já foi liberado.
+     */
+    private boolean released;
+
+    /**
+     * Constrói um buffer de upload.
+     *
+     * @param renderedBuffer Buffer renderizado.
+     */
     public UploadBuffer(TerrainBufferBuilder.RenderedBuffer renderedBuffer) {
         TerrainBufferBuilder.DrawState drawState = renderedBuffer.drawState();
         this.indexCount = drawState.indexCount();
         this.autoIndices = drawState.sequentialIndex();
         this.indexOnly = drawState.indexOnly();
 
+        /**
+         * Verifica se o buffer contém vértices.
+         */
         if (!this.indexOnly) {
-            this.buffer = Util.createDirectBuffer(renderedBuffer.vertexBuffer().capacity());
-            buffer.order(ByteOrder.nativeOrder());
-            buffer.put(renderedBuffer.vertexBuffer());
+            this.vertexBuffer = Util.createCopy(renderedBuffer.vertexBuffer());
         } else {
-            this.buffer = null;
+            this.vertexBuffer = null;
         }
 
+        /**
+         * Verifica se o buffer contém índices.
+         */
         if (!drawState.sequentialIndex()) {
-            this.buffer = Util.appendDirectBuffer(this.buffer, renderedBuffer.indexBuffer());
+            this.indexBuffer = Util.createCopy(renderedBuffer.indexBuffer());
+        } else {
+            this.indexBuffer = null;
         }
     }
 
-    public int indexCount() { return indexCount; }
+    /**
+     * Obtém o número de índices no buffer.
+     *
+     * @return Número de índices.
+     */
+    public int indexCount() {
+        return this.indexCount;
+    }
 
-    public ByteBuffer getBuffer() { return buffer; }
+    /**
+     * Obtém o buffer de vértices.
+     *
+     * @return Buffer de vértices.
+     */
+    public ByteBuffer getVertexBuffer() {
+        if (this.vertexBuffer == null) {
+            throw new IllegalStateException("O buffer de vértices não existe.");
+        }
+        return this.vertexBuffer;
+    }
 
+    /**
+     * Obtém o buffer de índices.
+     *
+     * @return Buffer de índices.
+     */
+    public ByteBuffer getIndexBuffer() {
+        if (this.indexBuffer == null) {
+            throw new IllegalStateException("O buffer de índices não existe.");
+        }
+        return this.indexBuffer;
+    }
+
+    /**
+     * Libera o buffer.
+     */
     public void release() {
-        if (buffer != null) {
-            MemoryUtil.memFree(buffer);
+        if (this.vertexBuffer != null) {
+            MemoryUtil.memFree(this.vertexBuffer);
+        }
+        if (this.indexBuffer != null) {
+            MemoryUtil.memFree(this.indexBuffer);
         }
         this.released = true;
     }
