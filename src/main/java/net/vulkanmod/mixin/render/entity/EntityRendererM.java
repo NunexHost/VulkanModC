5e3efc470c42adf70eaff11585faa95069113311
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-19 23:11:05 -0300

    Update EntityRendererM.java

--- src/main/java/net/vulkanmod/mixin/render/entity/EntityRendererM.java
+++ src/main/java/net/vulkanmod/mixin/render/entity/EntityRendererM.java
@@ -4,6 +4,7 @@ import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 import net.vulkanmod.Initializer;
 import net.vulkanmod.render.chunk.RenderSection;
 import net.vulkanmod.render.chunk.WorldRenderer;
@@ -14,46 +15,28 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(EntityRenderer.class)
 public class EntityRendererM<T extends Entity> {
 
-    /**
-     * @author
-     * @reason
-     */
-    @Overwrite
-    public boolean shouldRender(T entity, Frustum frustum, double d, double e, double f) {
-        if (!entity.shouldRender(d, e, f)) {
-            return false;
-        } else if (entity.noCulling) {
-            return true;
-        } else {
-            AABB aabb = entity.getBoundingBoxForCulling().inflate(0.5);
-            if (aabb.hasNaN() || aabb.getSize() == 0.0) {
-                aabb = new AABB(entity.getX() - 2.0, entity.getY() - 2.0, entity.getZ() - 2.0, entity.getX() + 2.0, entity.getY() + 2.0, entity.getZ() + 2.0);
-            }
-
+    @Redirect(method = "shouldRender", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/culling/Frustum;isVisible(Lnet/minecraft/world/phys/AABB;)Z"))
+    private boolean isVisible(Frustum frustum, AABB aabb) {
+        if(Initializer.CONFIG.entityCulling) {
             WorldRenderer worldRenderer = WorldRenderer.getInstance();
-            Vec3i pos = aabb.getCenter().toVec3i();
-            RenderSection section = worldRenderer.getSectionGrid().getSectionAt(pos);
 
-            if (section == null) {
-                // If the section is not found, assume the entity is visible.
-                return true;
-            } else if (section.isVisible() && section.getLastFrame() == worldRenderer.getLastFrame()) {
-                // If the section is visible and the entity has been rendered in this frame, it is still visible.
-                return true;
-            } else {
-                // If the section is not visible or the entity has not been rendered in this frame, it is not visible.
-                return false;
-            }
+            Vec3 pos = aabb.getCenter();
+
+            RenderSection section = worldRenderer.getSectionGrid().getSectionAtBlockPos((int) pos.x(), (int) pos.y(), (int) pos.z());
+
+            if(section == null)
+                return frustum.isVisible(aabb);
+
+            return worldRenderer.isChunkUpdatedInLastFrame(section);
+        } else {
+            return frustum.isVisible(aabb);
         }
     }
 
-    /**
-     * @author
-     * @reason
-     * This method is commented out because it performs a full frustum check even if the entity is within the bounding section. This is an unnecessary step.
-     */
-    @Redirect(method = "shouldRender", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/culling/Frustum;isVisible(Lnet/minecraft/world/phys/AABB;)Z"))
-    private boolean isVisible(Frustum frustum, AABB aabb) {
-        return false;
+    // Opcional: incorporar a lógica de visibilidade individual das entidades
+
+    @Redirect(method = "shouldRender", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;shouldRender(DD)Z"))
+    private boolean shouldRender(Entity entity, double d, double e, double f) {
+        return entity.shouldRender(d, e, f) && (Initializer.CONFIG.entityCulling ? worldRenderer.isChunkUpdatedInLastFrame(worldRenderer.getSectionGrid().getSectionAtBlockPos(entity.getBlockX(), entity.getBlockY(), entity.getBlockZ())) : true);
     }
 }
