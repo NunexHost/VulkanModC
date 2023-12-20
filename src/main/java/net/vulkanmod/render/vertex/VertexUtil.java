113a106c48c9c870e469ecd50fbaa0742ce0e275
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-20 00:40:34 -0300

    Update VertexUtil.java

--- src/main/java/net/vulkanmod/render/vertex/VertexUtil.java
+++ src/main/java/net/vulkanmod/render/vertex/VertexUtil.java
@@ -2,48 +2,39 @@ package net.vulkanmod.render.vertex;
 
 public class VertexUtil {
 
-    private static final float NORM_INV = 1.0f / 127.0f;
-    private static final float COLOR_INV = 1.0f / 255.0f;
+    private static final int NORM_SHIFT = 8;
+    private static final int COLOR_SHIFT = 24;
 
     public static int packColor(float r, float g, float b, float a) {
-        r *= 255.0f;
-        g *= 255.0f;
-        b *= 255.0f;
-        a *= 255.0f;
-
-        return ((int)r & 0xFF) | ((int)g & 0xFF) << 8 |  ((int)b & 0xFF) << 16 | ((int)a & 0xFF) << 24;
+        return (int)r << COLOR_SHIFT | (int)g << (COLOR_SHIFT - 8) | (int)b << (COLOR_SHIFT - 16) | (int)a;
     }
 
     public static int packNormal(float x, float y, float z) {
-        x *= 127.0f;
-        y *= 127.0f;
-        z *= 127.0f;
-
-        return ((int)x & 0xFF) | ((int)y & 0xFF) << 8|  ((int)z & 0xFF) << 16;
+        return (int)x << NORM_SHIFT | (int)y << (NORM_SHIFT - 8) | (int)z;
     }
 
     public static float unpackColorR(int i) {
-        return ((i >> 24) & 0xFF) * COLOR_INV;
+        return (i >> COLOR_SHIFT) & 0xFF;
     }
 
     public static float unpackColorG(int i) {
-        return ((i >> 16) & 0xFF) * COLOR_INV;
+        return (i >> (COLOR_SHIFT - 8)) & 0xFF;
     }
 
     public static float unpackColorB(int i) {
-        return ((i >> 8) & 0xFF) * COLOR_INV;
+        return (i >> (COLOR_SHIFT - 16)) & 0xFF;
     }
 
     public static float unpackN1(int i) {
-        return (byte)(i & 0xFF) * NORM_INV;
+        return (i >> NORM_SHIFT) & 0xFF;
     }
 
     public static float unpackN2(int i) {
-        return (byte)((i >> 8) & 0xFF) * NORM_INV;
+        return (i >> (NORM_SHIFT - 8)) & 0xFF;
     }
 
     public static float unpackN3(int i) {
-        return (byte)((i >> 16) & 0xFF) * NORM_INV;
+        return i & 0xFF;
     }
 
 }
